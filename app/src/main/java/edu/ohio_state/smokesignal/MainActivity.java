1d0796342ad1490222dbc37a16a41a8c793e6ba4
asadlabqi <asadiqbal99@hotmail.com>
2016-03-02 13:28:09 -0500

    Generation of PRNG added to MainActivity. Elements of activity_main were changed for debugging purposes.

--- app/src/main/java/edu/ohio_state/smokesignal/MainActivity.java
+++ app/src/main/java/edu/ohio_state/smokesignal/MainActivity.java
@@ -4,13 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.security.SecureRandom;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Array for converting byte array to printable format
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    SecureRandom mSecureRandom;
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSecureRandom = new SecureRandom();
+        byte[] output = new byte[16];
+        mSecureRandom.nextBytes(output);
+
+        mTextView = (TextView) findViewById(R.id.prng);
+        String prng = bytesToHex(output);
+        mTextView.setText(prng);
+
     }
 
     @Override
@@ -34,4 +51,18 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    //Converts byte array to a string of hex digits while keeping leading zeroes
+    //Source: http://stackoverflow.com/questions/9655181/how-to-convert-a-byte-array-to-a-hex-string-in-java
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
 }
+
