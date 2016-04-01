f7d233a4ea38816dc8a36dd689a0138dc772c16b
asadlabqi <asadiqbal99@hotmail.com>
2016-04-01 17:29:58 -0400

    WOW i hope im not breaking this

--- app/src/main/java/edu/ohio_state/smokesignal/MainActivity.java
+++ app/src/main/java/edu/ohio_state/smokesignal/MainActivity.java
@@ -1,6 +1,5 @@
 package edu.ohio_state.smokesignal;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.nfc.NdefMessage;
@@ -13,22 +12,21 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks,
-                    KeyBankFragment.OnKeySharedListener
- {
+        KeyBankFragment.OnKeySharedListener
+{
 
-     private String TAG = "Main Activity";
-     private NavigationDrawerFragment mNavigationDrawerFragment;
-     private CharSequence mTitle;
+    private String TAG = "Main Activity";
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private CharSequence mTitle;
 
     TextView mTextView;
 
@@ -55,63 +53,65 @@ public class MainActivity extends AppCompatActivity
         }
         // Register callback
         mNfcAdapter.setNdefPushMessageCallback(this, this);
-
         Log.d("MainActivity", "nfc adapter success!!");
         */
 
     }
 
-     //NAVIGATION DRAWER
-     @Override
-     public void onNavigationDrawerItemSelected(int position) {
-         // update the main content by replacing fragments
-         FragmentManager fragmentManager = getSupportFragmentManager();
+    //NAVIGATION DRAWER
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
-         Log.d(TAG, "Position: " + position);
+        Log.d(TAG, "Position: " + position);
 
-         Fragment fragment = PlaceholderFragment.newInstance(position + 1);
-         if (position == 0) {
-             fragment = EncryptionFragment.newInstance();
-         }else if(position == 1){
-             fragment = KeyExchangeFragment.newInstance(null);
-         }else if(position == 2){
-             // TODO: Create a Messages Fragment.
-         }
-         else if(position == 3){
-             fragment = KeyBankFragment.newInstance(null);
-         }
-         else {
-             // TODO: Create a Settings Fragment.
-         }
+        Fragment fragment;
+        if (position == 0) {
+            fragment = EncryptionFragment.newInstance();
+        }else if(position == 1){
+            fragment = KeyExchangeFragment.newInstance(null);
+        }else if(position == 2){
+            fragment = MessagesFragment.newInstance();
+        }
+        else if(position == 3){
+            fragment = KeyBankFragment.newInstance(null);
+        }
+        else {
+            fragment = SettingsFragment.newInstance();
+        }
 
-         fragmentManager.beginTransaction()
-                 .replace(R.id.container, fragment).commit();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, fragment).commit();
 
-     }
+    }
 
-     public void onSectionAttached(int number) {
-         switch (number) {
-             case 1:
-                 mTitle = getString(R.string.encryption);
-                 break;
-             case 2:
-                 mTitle = getString(R.string.share_key);
-                 break;
-             case 3:
-                 mTitle = getString(R.string.messages);
-                 break;
-             case 4:
-                 mTitle = getString(R.string.settings);
-                 break;
-         }
-     }
+    // TODO: Determine if this is still needed now that PlaceHolder Fragment is gone.
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.encryption);
+                break;
+            case 2:
+                mTitle = getString(R.string.share_key);
+                break;
+            case 3:
+                mTitle = getString(R.string.messages);
+                break;
+            case 4:
+                mTitle = getString(R.string.key_bank);
+                break;
+            case 5:
+                mTitle = getString(R.string.settings);
+                break;
+        }
+    }
 
-     public void restoreActionBar() {
-         ActionBar actionBar = getSupportActionBar();
-         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-         actionBar.setDisplayShowTitleEnabled(true);
-         actionBar.setTitle(mTitle);
-     }
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -142,103 +142,49 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-     // TODO: Determine if we still need this code. Otherwise delete.
-     /*
-     @Override
-     public NdefMessage createNdefMessage(NfcEvent event) {
-         String text = ("Beam me up, Android!\n\n" +
-                 "Beam Time: " + System.currentTimeMillis());
-         NdefMessage msg = new NdefMessage(
-                 new NdefRecord[] { createMime(
-                         "application/edu.ohio_state.smokesignal", text.getBytes())
-                 });
-         return msg;
-     }
+    @Override
+    public void onNewIntent(Intent intent) {
+        // onResume gets called after this to handle the intent
+        setIntent(intent);
+    }
+
+    /**
+     * Parses the NDEF Message from the intent and shows it on a Toast. The Activity then reloads
+     * the KeyBank so that the user can see their new key.
      */
+    void processIntent(Intent intent) {
+        Log.d(TAG, "NDEF Intent is being processed.");
+        mTextView = (TextView) findViewById(R.id.key_exchange_text);
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                NfcAdapter.EXTRA_NDEF_MESSAGES);
+        // only one message sent during the beam
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        // record 0 contains the MIME type, record 1 is the AAR, if present
+        byte[] messageText = msg.getRecords()[0].getPayload();
+        Toast.makeText(MainActivity.this, Arrays.toString(messageText), Toast.LENGTH_SHORT).show();
 
-     @Override
-     public void onNewIntent(Intent intent) {
-         // onResume gets called after this to handle the intent
-         setIntent(intent);
-     }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = KeyBankFragment.newInstance(messageText);
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, fragment).commit();
+    }
 
-     /**
-      * Parses the NDEF Message from the intent and shows it on a Toast. The Activity then reloads
-      * the KeyBank so that the user can see their new key.
-      */
-     void processIntent(Intent intent) {
-         Log.d(TAG, "NDEF Intent is being processed.");
-         Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
-                 NfcAdapter.EXTRA_NDEF_MESSAGES);
-         // only one message sent during the beam
-         NdefMessage msg = (NdefMessage) rawMsgs[0];
-         // record 0 contains the MIME type, record 1 is the AAR, if present
-         byte[] messageText = msg.getRecords()[0].getPayload();
-         Toast.makeText(MainActivity.this, messageText.toString(), Toast.LENGTH_SHORT).show();
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Check to see that the Activity started due to an Android Beam
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }
+    }
 
-         FragmentManager fragmentManager = getSupportFragmentManager();
-         Fragment fragment = KeyBankFragment.newInstance(messageText);
-         fragmentManager.beginTransaction()
-                 .replace(R.id.container, fragment).commit();
-     }
-
-     @Override
-     public void onResume() {
-         super.onResume();
-         // Check to see that the Activity started due to an Android Beam
-         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-             processIntent(getIntent());
-         }
-     }
-
-     // Called when the user selects a key to be shared in the Key Bank Fragment.
-     @Override
-     public void OnKeyShared(Uri uri) {
-         Log.d(TAG, "Key received from Key Bank. URI = " + uri.toString());
-         FragmentManager fragmentManager = getSupportFragmentManager();
-         Fragment fragment = KeyExchangeFragment.newInstance(uri);
-         fragmentManager.beginTransaction()
-                 .replace(R.id.container, fragment).commit();
-     }
-
-     /**
-      * A placeholder fragment containing a simple view.
-      */
-     public static class PlaceholderFragment extends Fragment {
-         /**
-          * The fragment argument representing the section number for this
-          * fragment.
-          */
-         private static final String ARG_SECTION_NUMBER = "section_number";
-
-         /**
-          * Returns a new instance of this fragment for the given section
-          * number.
-          */
-         public static PlaceholderFragment newInstance(int sectionNumber) {
-             PlaceholderFragment fragment = new PlaceholderFragment();
-             Bundle args = new Bundle();
-             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-             fragment.setArguments(args);
-             return fragment;
-         }
-
-         public PlaceholderFragment() {
-         }
-
-         @Override
-         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                  Bundle savedInstanceState) {
-             return inflater.inflate(R.layout.fragment_main, container, false);
-         }
-
-         @Override
-         public void onAttach(Activity activity) {
-             super.onAttach(activity);
-             ((MainActivity) activity).onSectionAttached(
-                     getArguments().getInt(ARG_SECTION_NUMBER));
-         }
-     }
-
+    // Called when the user selects a key to be shared in the Key Bank Fragment.
+    @Override
+    public void OnKeyShared(Uri uri) {
+        Log.d(TAG, "Key received from Key Bank. URI = " + uri.toString());
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = KeyExchangeFragment.newInstance(uri);
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, fragment).commit();
+    }
 }
-
